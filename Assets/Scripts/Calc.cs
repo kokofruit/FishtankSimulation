159a9d628555c4b3682b36d3a87ca5d89d048cf5
GregStevensonJeffersonIII <sandlesjames23@gmail.com>
2026-04-01 12:21:38 -0400

    getting there soon
    itty bit morem

--- Assets/Scripts/Calc.cs
+++ Assets/Scripts/Calc.cs
@@ -6,16 +6,23 @@ using UnityEngine.UIElements;
 public class Calc : MonoBehaviour
 {
     HashSet<string> _goodFish;
-    Dictionary<string,Fish> _allFish;
+    Dictionary<string,FishButton> _allFish;
 
-    public GameObject buttonPrefab;
+    public static GameObject buttonPrefab;
 
-    class Fish
+    public static Vector3 stupid_temp = new Vector3(0, 0, 0);
+
+    class FishButton
     {
-        public HashSet<string> friends;
+        public JSONReader.Fish fish;
         public GameObject button;
 
-        private Vector3 stupid_temp = new Vector3(0,0,0);
+        public FishButton(JSONReader.Fish f) { 
+            fish = f;
+            button = Instantiate(buttonPrefab, stupid_temp, Quaternion.identity);
+            stupid_temp.y++;
+        }
+
     
     }
 
@@ -24,11 +31,13 @@ public class Calc : MonoBehaviour
     {
         //load fish from json here into all fish and good fish because all fish are good fish if you think about it really  im not trying to get like emotional or anything here its just the fish deserve better and i feel you guys take them for granted  fish are important for the ecosystem yknow    i got off track   fish are good      for now
         //also make the buttons here
-        foreach (var fish in _allFish)
-        { 
-            
-        }
-        
+        _allFish = new Dictionary<string, FishButton>();
+        _goodFish = new HashSet<string>();
+        foreach (var fish in SimulationManager.instance.fishInventory)
+        {
+            _allFish.Add(fish.name,new FishButton(fish));
+            _goodFish.Add(fish.name);
+        }        
     }
 
 
@@ -37,7 +46,27 @@ public class Calc : MonoBehaviour
         if (!_goodFish.Contains(bob))
             return;
 
-        _goodFish.Intersect(_allFish[bob].friends);
+        _goodFish.Intersect(_allFish[bob].fish.friends);
+    }
+
+    public void removeFish(string bob)
+    {
+        _allFish.Remove(bob);
+
+        _goodFish.Clear();
+
+        foreach (var fish in _allFish)
+        {
+            if (!_goodFish.Contains(fish.Key))
+            {
+                fish.Value.button.SetActive(false);
+            }
+            else
+            {
+                //pro
+                fish.Value.button.SetActive(true);
+            }
+        }
     }
 
     public void theTing() {
